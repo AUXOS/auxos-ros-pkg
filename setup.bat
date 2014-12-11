2b35c02dee7a98d4bc1726413024396d0c3348bc
David Hodo <david.hodo@is4s.com>
2014-12-10 23:19:31 -0600

    added setup batch file

---
+++ setup.bat
@@ -0,0 +1,4 @@
+
+@call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat" /x86 /Release
+@set PATH=%PATH%;C:\Program Files (x86)\Git\bin
+@call "C:\opt\ros\hydro\x86\setup.bat"
